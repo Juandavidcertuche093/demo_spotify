2abbf377b51ce98e81c3e1f23c77e8aec4635b30
juandavidcertuche93 <juandavidcertuchequijano2@gmail.com>
2024-05-22 11:43:48 -0500

    generamos modulos, creamos las rutas primcipales, genereamos el modulo de la carpeta shared y sus comopoenetes

---
+++ src/app/shared/components/media-play/media-play.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-media-play',
+  standalone: true,
+  imports: [],
+  templateUrl: './media-play.component.html',
+  styleUrl: './media-play.component.css'
+})
+export class MediaPlayComponent {
+
+}
