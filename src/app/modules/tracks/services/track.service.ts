3f2c5399aecbdf62e0971f9cc09ced072e3d76fa
juandavidcertuche93 <juandavidcertuchequijano2@gmail.com>
2024-05-25 19:43:35 -0500

    configurando la ruta de la api para la conexion

--- src/app/modules/tracks/services/track.service.ts
+++ src/app/modules/tracks/services/track.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
-import { TrackModel } from '../../../core/models/tracks.model';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../../environments/environments';
 
