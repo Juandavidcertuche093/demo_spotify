f17dca045cd8cbb9dc9efc82f692be49d7c58d48
juandavidcertuche93 <juandavidcertuchequijano2@gmail.com>
2024-05-27 15:10:12 -0500

    configuramos el app.routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,15 +2,17 @@ import { Routes,  RouterModule } from '@angular/router';
 import { NgModule } from "@angular/core";
 import { HomePagesComponent } from './modules/home/pages/home-pages/home-pages.component';
 
+
 export const routes: Routes = [
     {
-        path: 'auth', //
+        path: 'auth', // (public)
         loadChildren:() => import('./modules/auth/auth.module'). then((m) => m.AuthModule) //carga perezosa
     },
     {
-        path: '', // localhost:4200/ esta seria la raiz del proyecto
+        path: '', // (privada) localhost:4200/ esta seria la raiz del proyecto 
         component: HomePagesComponent, //compoenete principal para esta ruta
-        loadChildren:() => import('./modules/home/home.module'). then((m) => m.HomeModule) //carga perezosa
+        loadChildren:() => import('./modules/home/home.module'). then((m) => m.HomeModule), //carga perezosa
+        canActivate: []
     }
 ];
 
